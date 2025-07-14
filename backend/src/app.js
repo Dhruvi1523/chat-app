eb28e5aec0019cc332ce9367d681700238d11d18
Dhruvi1523 <[jingardhruvi2365gmail.com]>
2025-07-14 12:19:33 +0530

    add frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ limit: "50mb"  , extended: true }));
 app.use(cors(
    {
-    origin : "http://localhost:5173" ,
+    origin : "http://localhost:5173" || "https://chat-app-1-9s03.onrender.com",
     credentials : true ,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization"
